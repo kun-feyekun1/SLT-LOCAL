0b34a81679a497e123f63670cc703bd1de213ff0
Mohammed Hassen Ahmed <mohammed.ugr-2340-16@aau.edu.et>
2026-07-26 12:16:17 -0200

    src/design-system/index.ts modified

--- src/design-system/index.ts
+++ src/design-system/index.ts
@@ -1,18 +1,21 @@
-
 /*
    SmartLink Transit Design System
    Central export for all design system modules
  */
 
-export * from './components';
+export * from "./components";
 export type {
   AvatarSize,
-  BadgeColor, ButtonSize, ButtonVariant, CardPadding, CardVariant, FABSize,
-  FABVariant, InputState, SnackbarVariant
-} from './components';
-export * from './hooks/animation';
-export * from './theme';
-export type { Theme, ThemeMode } from './theme';
-export * from './tokens';
-export * from './utils';
-
+  BadgeColor,
+  CardPadding,
+  CardVariant,
+  FABSize,
+  FABVariant,
+  InputState,
+  SnackbarVariant,
+} from "./components";
+export * from "./hooks/animation";
+export * from "./theme";
+export type { Theme, ThemeMode } from "./theme";
+export * from "./tokens";
+export * from "./utils";
