55026c7c42a56ea2cecf00cb59ed3416ab24c6e9
Mohammed Hassen Ahmed <mohammed.ugr-2340-16@aau.edu.et>
2026-08-03 17:31:19 -0200

    providers and types modified

--- src/providers/ReduxProvider.tsx
+++ src/providers/ReduxProvider.tsx
@@ -1,45 +1,3 @@
-// // src/providers/ReduxProvider.tsx
-// import React, { type ReactNode } from "react";
-// import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
-
-// import { LoadingSpinner } from "@/components/index";
-// import { persistor, store } from "@/store";
-
-// export const ReduxProvider = ({ children }: { children: ReactNode }) => {
-//   return (
-//     <Provider store={store}>
-//       <PersistGate loading={<LoadingSpinner />} persistor={persistor}>
-//         {children}
-//       </PersistGate>
-//     </Provider>
-//   );
-// };
-
-// import React from 'react';
-// import { ActivityIndicator, View } from 'react-native';
-// import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { persistor, store } from '@/store'; // Adjust import path if needed
-
-// export function ReduxProvider({ children }: { children: React.ReactNode }) {
-//   return (
-//     <Provider store={store}>
-//       <PersistGate
-//         loading={
-//           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//             <ActivityIndicator color="#007AFF" size="large" />
-//           </View>
-//         }
-//         persistor={persistor}
-//       >
-//         {children}
-//       </PersistGate>
-//     </Provider>
-//   );
-// }
-
-import { ReduxHydrationLoading } from "@/providers/components/ReduxHydrationLoading";
 import { persistor, store } from "@/store";
 import type { PropsWithChildren } from "react";
 import { Provider } from "react-redux";
@@ -47,7 +5,7 @@ import { PersistGate } from "redux-persist/integration/react";
 export function ReduxProvider({ children }: PropsWithChildren) {
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor} loading={<ReduxHydrationLoading />}>
+      <PersistGate persistor={persistor} loading={null}>
         {children}
       </PersistGate>
     </Provider>
